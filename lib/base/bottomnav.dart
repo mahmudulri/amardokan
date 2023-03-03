2dbd4d9927ce68625e365c8ca8ffdc90acece729
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2023-03-04 00:01:06 +0600

    asdfa

--- lib/base/bottomnav.dart
+++ lib/base/bottomnav.dart
@@ -30,7 +30,186 @@ class _BottomNavPageState extends State<BottomNavPage> {
     var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       extendBody: true,
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: screenHeight * 0.070,
+              ),
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.blue,
+              ),
+              SizedBox(
+                height: screenHeight * 0.010,
+              ),
+              Text(
+                "MD Mahmudul Hasan",
+                style: GoogleFonts.acme(
+                  fontSize: screenHeight * 0.022,
+                ),
+              ),
+              Text(
+                "Admin",
+                style: GoogleFonts.acme(
+                  fontSize: screenHeight * 0.018,
+                ),
+              ),
+              Text(
+                "01701987948",
+                style: GoogleFonts.acme(
+                  fontSize: screenHeight * 0.022,
+                ),
+              ),
+              Divider(
+                thickness: 2,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: screenHeight * 0.050,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "My Statistic",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.022,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight * 0.050,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "All Stuffs Statistic",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.022,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Only for admin",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.012,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight * 0.025,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Stuff-1",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.022,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight * 0.025,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Stuff-2",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.022,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: screenHeight * 0.025,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Stuff-3",
+                    style: GoogleFonts.acme(
+                      fontSize: screenHeight * 0.022,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Container(
+                    // width: screenWidth * 0.350,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.logout,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Log Out",
+                            style: GoogleFonts.acme(
+                              fontSize: screenHeight * 0.022,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle(
           // Status bar color
