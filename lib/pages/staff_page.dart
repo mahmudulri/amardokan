46a72477de054c422754a1ec2623677e2dad978e
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2023-03-07 22:52:00 +0600

    added staff page

--- lib/pages/staff_page.dart
+++ lib/pages/staff_page.dart
@@ -1,14 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StaffPage extends StatelessWidget {
   const StaffPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Staff Page"),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "All Staffs",
+                  style: GoogleFonts.oswald(
+                    fontSize: screenWidth * 0.045,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.030,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.005,
+            ),
+            Container(
+              height: screenHeight * 0.40,
+              width: screenWidth,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey,
+              ),
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Mahmudul Hasan",
+                              style: GoogleFonts.roboto(
+                                fontSize: screenWidth * 0.035,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              "Staff ID",
+                              style: GoogleFonts.roboto(
+                                fontSize: screenWidth * 0.035,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.02,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Total Customer : 1205",
+                  style: GoogleFonts.oswald(
+                    fontSize: screenWidth * 0.045,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.030,
+                ),
+                Text(
+                  "Add New Customer",
+                  style: GoogleFonts.oswald(
+                    fontSize: screenWidth * 0.025,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.030,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
