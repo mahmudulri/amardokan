b907f333ba10dbf214c41054c8c8a220974ab285
MD. Mahmudul Hasan <mahmudul.ri@gmail.com>
2023-03-07 10:58:55 +0600

    added user list

--- lib/pages/customerlist.dart
+++ lib/pages/customerlist.dart
@@ -1,14 +1,233 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomerList extends StatelessWidget {
   const CustomerList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Staff Page"),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            Container(
+              height: screenHeight * 0.060,
+              width: screenWidth,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      "search by name or id",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "Filter by village",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenHeight * 0.010,
+            ),
+            Expanded(
+              child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                separatorBuilder: (context, index) {
+                  return SizedBox(
+                    height: screenHeight * 0.02,
+                  );
+                },
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ClipPath(
+                    clipper: SideCutClipper(),
+                    child: Container(
+                      height: 200,
+                      width: 500,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey[300],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "USER ID: 20938",
+                                          style: GoogleFonts.oswald(
+                                            fontSize: screenWidth * 0.035,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "NAME: Mahmudul Hasan",
+                                          style: GoogleFonts.oswald(
+                                            fontSize: screenWidth * 0.035,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "Phone: 01701987948",
+                                          style: GoogleFonts.oswald(
+                                            fontSize: screenWidth * 0.035,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "VILL: Kadirpara",
+                                          style: GoogleFonts.oswald(
+                                            fontSize: screenWidth * 0.035,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Spacer(),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "BALANCE: - 870 tk",
+                                          style: GoogleFonts.oswald(
+                                            fontSize: screenWidth * 0.035,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 20),
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            "Send SMS",
+                                            style: GoogleFonts.oswald(
+                                              fontSize: screenWidth * 0.035,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w400,
+                                            ),
+                                          ),
+                                          Spacer(),
+                                          Icon(
+                                            Icons.mail,
+                                            color: Colors.black,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 100,
+                              width: 2,
+                              color: Colors.grey,
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                shape: BoxShape.circle),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(12.0),
+                                              child: Icon(Icons.add),
+                                            ),
+                                          ),
+                                          Spacer(),
+                                          Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                shape: BoxShape.circle),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(12.0),
+                                              child: Icon(Icons.remove),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Spacer(),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: Colors.white,
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(12.0),
+                                          child: Text(
+                                            "History",
+                                            style: GoogleFonts.oswald(
+                                              fontSize: screenWidth * 0.035,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w400,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
